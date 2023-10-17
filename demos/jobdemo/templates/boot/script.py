0d2fb4d6c843a6df76fc7306c213d48cf70bc78e
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2023-10-17 14:30:12 +0200

    Avoid submission of Python scripts with sbatch
    Fixes #12

---
+++ demos/jobdemo/templates/boot/script.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import json
+import time
+
+
+def main():
+    print("loading kwargs file")
+    with open("kwargs.json") as f:
+        kwargs = json.load(f)
+
+    print("looping")
+    configs = []
+    for i in range(kwargs["boot_size"]):
+        fn_out = f"config_{i:03d}.txt"
+        with open(fn_out, "w") as f:
+            f.write(f"Bla bla {i}\n")
+        configs.append(fn_out)
+
+    time.sleep(kwargs["pause"])
+
+    print("writing result file")
+    with open("result.json", "w") as f:
+        json.dump(configs, f)
+
+
+if __name__ == "__main__":
+    main()
---
+++ demos/jobdemo/templates/sample/script.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import json
+import time
+from pathlib import Path
+
+
+def main():
+    print("loading kwargs file")
+    with open("kwargs.json") as f:
+        kwargs = json.load(f)
+
+    print("loading models")
+    for model in kwargs["models"]:
+        # Just checking the presence of the file.
+        # A Realistic example would load and use it.
+        assert Path(model).is_file()
+
+    print("sampling new configurations")
+    configs = []
+    for i in range(kwargs["sample_size"]):
+        fn_out = f"config_{i:03d}.txt"
+        with open(fn_out, "w") as f:
+            f.write(f"Bla bla {i} at temperature {kwargs['temperature']}\n")
+        configs.append(fn_out)
+
+    time.sleep(kwargs["pause"])
+
+    print("writing result file")
+    with open("result.json", "w") as f:
+        json.dump(configs, f)
+
+
+if __name__ == "__main__":
+    main()
